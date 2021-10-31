4c3d6c2ae7aafd8f3b75735fe984c07a0d4b76c9
bmalbright <brianalbright@utexas.edu>
2021-10-31 18:45:13 -0500

    added the file structure. used some class lessons as models for code.

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Project = require('./Project');
+const Comments = require('./Comments');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Project.hasMany(Comments, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+ 
+Comments.belongsTo(Project, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project, Comments };

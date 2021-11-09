1288fc9991e413e3fcb055721bd2acd433b35482
bmalbright <brianalbright@utexas.edu>
2021-11-09 17:44:41 -0600

    added small changes across many files.

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,9 +1,8 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#comment-name').value.trim();
-   // const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#comment-desc').value.trim();
+
+    const description = document.querySelector('#comment').value.trim();
   
     if (name && description) {
       const response = await fetch(`/api/comments`, {
